d219815e6661377b26e753e1ecfd1cf4a39a08bf
seohuibae <seohuibae73@gmail.com>
2020-04-01 08:29:04 +0900

    modify vae

--- vae_model.py
+++ vae_model.py
@@ -13,41 +13,23 @@ class VAEModel(nn.Module):
         self.device = params['device']
         self.channel = params['channel']
 
-        self.fc1 = nn.Linear(self.A*self.A, 400)
-        self.fc21 = nn.Linear(400, self.z_size)
-        self.fc22 = nn.Linear(400, self.z_size)
-        self.fc3 = nn.Linear(self.z_size, 400)
-        self.fc4 = nn.Linear(400, self.A*self.A)
-
     def encode(self, x):
-        h1 = F.relu(self.fc1(x))
-        return self.fc21(h1), self.fc22(h1)
+        h# TODO 
+        return None
 
     def reparameterize(self, mu, logvar):
-        std = torch.exp(0.5*logvar)
-        eps = torch.randn_like(std)
-        return mu + eps*std
+        # TODO 
+        return None
 
     def decode(self, z):
-        h3 = F.relu(self.fc3(z))
-        return torch.sigmoid(self.fc4(h3))
+        # TODO 
+        return None
 
     def forward(self, x):
-        mu, logvar = self.encode(x.view(-1, 784))
-        z = self.reparameterize(mu, logvar)
-        return self.decode(z), mu, logvar
+        # TODO
+        return None
 
     def loss(self, x):
-        
-
-        x_recon, mu, logvar= self.forward(x)
-        Lx = F.binary_cross_entropy(x_recon, x.view(-1, 784), reduction='sum')
-
-        # see Appendix B from VAE paper:
-        # Kingma and Welling. Auto-Encoding Variational Bayes. ICLR, 2014
-        # https://arxiv.org/abs/1312.6114
-        # 0.5 * sum(1 + log(sigma^2) - mu^2 - sigma^2)
-        Ld = -0.5 * torch.sum(1 + logvar - mu.pow(2) - logvar.exp())
-
-        return Lx + Ld 
+        # TODO
+        return None
     
